b0bd59e8a6805ef44e9336d3425a590e0dc2b06e
Jordan Reed <jordanreed03@gmail.com>
2021-01-06 03:53:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,19 +7,18 @@ class CashRegister
     @discount = discount
   end 
   
- def add_item(title, price, count = 1)
-    if count > 1
+  def add_item(title,price,quantity = 1)
+    if quantity>1
       i=0
-      while i< count 
+      while i<quantity
         @items << title
         i+=1
       end
     else
       @items << title
     end
-    @total += price*count
+    @total += price*quantity
     @last_transaction_amount = @total
     @total
   end
- 
-end 
+end
