5451686fd4a0fd67a85170b46c9898604b43bdd3
Jordan Reed <jordanreed03@gmail.com>
2021-01-06 03:33:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,10 +8,8 @@ class CashRegister
   end 
   
   def add_item(total, price)
-    @total = total.to_i + price.to_i 
+    @total = total.to_i + price
   end 
   
-  def apply_discount 
-    self.total -= 1
-  end 
+ 
 end 
